e4787963b707512de303714499d13a846138452b
Veselin Romić <omegavesko@gmail.com>
2019-09-05 22:34:16 +0200

    Add PHP + extensions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+add-apt-repository ppa:ondrej/php
 apt-get update
+
 apt-get install -y docker-compose
 apt-get install -y fish
+
+apt-get install -y php7.3
+apt-get install -y php7.3-bcmath php7.3-bz2 php7.3-cgi php7.3-cli php7.3-curl php7.3-dba php7.3-enchant php7.3-fpm php7.3-gd php7.3-gmp php7.3-imap php7.3-interbase php7.3-intl php7.3-json php7.3-ldap php7.3-mbstring php7.3-mysql php7.3-odbc php7.3-opcache php7.3-pgsql php7.3-pspell php7.3-readline php7.3-recode php7.3-snmp php7.3-soap php7.3-sqlite3 php7.3-sybase php7.3-tidy php7.3-xml php7.3-xmlrpc php7.3-xsl php7.3-zip 
 
 useradd -m -s /usr/bin/fish -U veselin -u 666
 cp -pr /home/vagrant/.ssh /home/veselin/
