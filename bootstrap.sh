bfac2c9b74878aba72e21605151293b5b74f17a9
AntonioCS <antoniocs@gmail.com>
2019-09-20 07:14:15 +0100

    Removed a lot of unneeded configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Add repositories + `apt-get update`
-add-apt-repository ppa:ondrej/php # PHP
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - # Yarn
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list # Yarn
-curl -sL https://deb.nodesource.com/setup_12.x | bash - # Node
 apt-get update
 
 # Install Docker Compose
@@ -12,44 +7,16 @@ curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compo
 chmod +x /usr/local/bin/docker-compose
 
 # Initialize a Docker Swarm (some Docker Compose setups need it)
-docker swarm init
-
-# Install fish 
-apt-get install -y fish
-chsh -s /usr/bin/fish vagrant
-
-# Install PHP + extensions
-apt-get install -y php7.3
-apt-get install -y php7.3-bcmath php7.3-bz2 php7.3-cgi php7.3-cli php7.3-curl php7.3-dba php7.3-enchant php7.3-fpm php7.3-gd php7.3-gmp php7.3-imap php7.3-interbase php7.3-intl php7.3-json php7.3-ldap php7.3-mbstring php7.3-mysql php7.3-odbc php7.3-opcache php7.3-pgsql php7.3-pspell php7.3-readline php7.3-recode php7.3-snmp php7.3-soap php7.3-sqlite3 php7.3-sybase php7.3-tidy php7.3-xml php7.3-xmlrpc php7.3-xsl php7.3-zip 
-
-# Install Composer
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mv composer.phar /bin/composer
-
-# Install Node and Yarn
-apt-get install -y nodejs
-apt-get install -y yarn
-
-# Install Symfony CLI
-wget https://get.symfony.com/cli/installer -O - | bash
-mv /root/.symfony/bin/symfony /usr/local/bin/symfony
+#docker swarm init
 
 # Install misc. packages
-apt-get install -y unzip
+apt-get install -y unzip build-essential
 
 # https://docs.docker.com/install/linux/linux-postinstall/
 usermod -aG docker vagrant
 
 # Disable the Ubuntu MOTD
 touch /home/vagrant/.hushlogin
-
-# Disable the default fish greeting
-mkdir -p /home/vagrant/.config/fish/
-echo "set fish_greeting" >> /home/vagrant/.config/fish/config.fish
-chown -R vagrant:vagrant /home/vagrant/.config/
 
 # .dircolors
 cp /dev_config/.dircolors /home/vagrant/.dircolors
@@ -60,28 +27,5 @@ apt-get install -y resolvconf
 echo "nameserver 192.168.110.1" >> /etc/resolvconf/resolv.conf.d/head
 service resolvconf restart
 
-# Git config
-su -c "git config --global credential.helper store" vagrant
-su -c "git config --global user.name \"Veselin Romić\"" vagrant
-su -c "git config --global user.email \"veselin.romic@infostud.com\"" vagrant
-
 # Increase the inotify watcher limit 
-echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
-
-# Create ~/code, since unison won't do it for us
-mkdir -p /home/vagrant/code/
-chmod -R 777 /home/vagrant/code/
-chown -R vagrant:vagrant /home/vagrant/code/
-
-# Build unison from source
-apt-get install -y build-essential
-apt-get install -y ocaml
-mkdir -p /root/unison-build/
-cd /root/unison-build/
-wget -O source.tar.gz https://github.com/bcpierce00/unison/archive/v2.51.2.tar.gz
-tar xzf source.tar.gz
-cd unison-2.51.2/
-make UISTYLE=text
-cp /root/unison-build/unison-2.51.2/src/unison /bin/unison
-cp /root/unison-build/unison-2.51.2/src/unison-fsmonitor /bin/unison-fsmonitor
-rm -rf /root/unison-build/
+#echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
